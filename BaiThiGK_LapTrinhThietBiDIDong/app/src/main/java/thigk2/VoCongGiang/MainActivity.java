990a17b2cb2612982f5f1dda79ccc931c6f64326
vcgiang09h <vcgiang09@gmail.com>
2026-04-20 07:20:51 +0700

    COMMIT LẦN 0

---
+++ BaiThiGK_LapTrinhThietBiDIDong/app/src/main/java/thigk2/VoCongGiang/MainActivity.java
@@ -0,0 +1,12 @@
+package thigk2.VoCongGiang;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+}
